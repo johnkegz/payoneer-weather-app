e7da38b73d1b59a46085025d017a42e96119c96c
johnkegz <johnkal24@gmail.com>
2021-06-06 23:35:24 +0300

    change http to https

--- src/redux/actions/weatherInfoActions.js
+++ src/redux/actions/weatherInfoActions.js
@@ -6,7 +6,7 @@ import {
 
 export const getWeatherInfo = () => async (dispatch) => {
     dispatch({type: GET_WEATHER_REQUEST});
-    const url = process.env.REACT_APP_OPEN_WEATHER_MAP_URL || "http://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40";
+    const url = process.env.REACT_APP_OPEN_WEATHER_MAP_URL || "https://api.openweathermap.org/data/2.5/forecast?q=Munich,de&APPID=75f972b80e26f14fe6c920aa6a85ad57&cnt=40";
     return fetch(url)
     .then(response => response.json())
     .then(data => {
